5ee4580f730a271244b9b40c3d10f5f43765bc04
Rajath Shashidhara <rajaths.rajaths@gmail.com>
2015-02-12 21:04:26 +0530

    Add Distribution Counting Sort

--- sort.c
+++ sort.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 #include "sort.h"
 
 //Does not sort in-place.
@@ -7,6 +8,10 @@
 //N^2 worst, average, best time.
 void sortByComparisonCounting(int** keys, int N)
 {
+	assert(N>0);
+	assert(keys!=NULL);
+	assert(*keys!=NULL);
+
 	int* count = (int*) calloc(N, sizeof(int));
 	int* sortedKeys = (int*) calloc(N, sizeof(int));
 	int i,j;
@@ -33,7 +38,40 @@ void sortByComparisonCounting(int** keys, int N)
 	*keys = sortedKeys;
 }
 
+//startN<=keys[i]<=endN for all i belonging to [0,N).
+//Array keys is replaced by the new memory block containing sorted keys.
+//Linear running time.
+//Not a comparison sort.
+//Stable.
 void sortByDistributionCounting(int** keys, int N, int startN, int endN)
 {
-	
+	assert(N>0);
+	assert(endN>=startN);
+	assert(keys!=NULL);
+	assert(*keys!=NULL);
+
+	int* count = (int*)	calloc(endN-startN+1, sizeof(int));
+	int* sortedKeys = (int*) calloc(N, sizeof(int));
+	int i;
+
+	for(i=0; i<N; i++)
+	{
+		count[(*keys[i])-startN] += 1;
+	}
+
+	for(i=1; i<endN-startN+1; i++)
+	{
+		count[i] = count[i] + count[i-1];
+	}
+
+	for(i=N; i>=0; i--)
+	{
+		sortedKeys[count[(*keys[i])-startN]-1] = *keys[i];
+		count[(*keys[i])-startN] -= 1;
+	}
+
+	free(count);
+	free(*keys);
+
+	*keys = sortedKeys;
 }
--- sort.h
+++ sort.h
@@ -1,4 +1,5 @@
 #ifndef SORTING_TECHNIQUES
 #define SORTING_TECHNIQUES
 void sortByComparisonCounting(int** keys, int N);
+void sortByDistributionCounting(int** keys, int N, int startN, int endN);
 #endif
